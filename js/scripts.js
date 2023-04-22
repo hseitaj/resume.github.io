f43b51bb43e3353cc79553ea7c990b0f64ba6679
Hansi Seitaj <67917151+hseitaj@users.noreply.github.com>
2023-04-22 13:38:45 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,29 @@
 // Scripts
 // 
 
-document.getElementById('status').innerHTML = "Sending...";
-formData = {
-  'name': $('input[name=name]').val(),
-  'email': $('input[name=email]').val(),
-  'subject': $('input[name=subject]').val(),
-  'message': $('textarea[name=message]').val()
-};
+// document.getElementById('status').innerHTML = "Sending...";
+// formData = {
+//   'name': $('input[name=name]').val(),
+//   'email': $('input[name=email]').val(),
+//   'subject': $('input[name=subject]').val(),
+//   'message': $('textarea[name=message]').val()
+// };
 
 
-$.ajax({
-  url: "mail.php",
-  type: "POST",
-  data: formData,
-  success: function (data, textStatus, jqXHR) {
+// $.ajax({
+//   url: "mail.php",
+//   type: "POST",
+//   data: formData,
+//   success: function (data, textStatus, jqXHR) {
 
-    $('#status').text(data.message);
-    if (data.code) //If mail was sent successfully, reset the form.
-      $('#contact-form').closest('form').find("input[type=text], textarea").val("");
-  },
-  error: function (jqXHR, textStatus, errorThrown) {
-    $('#status').text(jqXHR);
-  }
-});
+//     $('#status').text(data.message);
+//     if (data.code) //If mail was sent successfully, reset the form.
+//       $('#contact-form').closest('form').find("input[type=text], textarea").val("");
+//   },
+//   error: function (jqXHR, textStatus, errorThrown) {
+//     $('#status').text(jqXHR);
+//   }
+// });
 
 window.addEventListener('DOMContentLoaded', event => {
 
